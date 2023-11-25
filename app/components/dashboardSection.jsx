c553ad74f99f0114ad6abc257d21d7247bdf66ca
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-25 22:25:26 +0530

    commit

---
+++ app/components/dashboardSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardSession = () => {
+  return (
+    <div>DashboardSession</div>
+  )
+}
+
+export default DashboardSession
---
+++ app/components/sidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="w-[15vw] h-screen bg-[#191717] text-white">
+      <div className="p-[2vw]">
+        <h1 className="text-white font-serif text-sm">
+          ECOMM <span className="text-2xl">DATA</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
