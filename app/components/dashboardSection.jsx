db14dd8b1a866f00e4ef62f86fdf732f14f9cc95
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-26 15:54:37 +0530

    final commit

--- app/components/dashboardSection.jsx
+++ app/components/dashboardSection.jsx
@@ -1,35 +1,23 @@
-
 import React from "react";
 import { TbReportSearch } from "react-icons/tb";
 import { CiSaveDown1 } from "react-icons/ci";
-
 import ProductsDeals from "./ProductsDeals";
 import Hamburger from "./Hamburger";
-
-
-
-import "bootstrap/dist/css/bootstrap.min.css"; 
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const DashboardSession = () => {
- 
   return (
-    <div className="md:w-[85vw] w-full h-screen bg-[#EEEEEE] overflow-auto  ">
+    <div className="md:w-[85vw] w-full h-screen bg-[#EEEEEE] overflow-auto  cursor-pointer">
       <div className="md:py-[1vw] md:px-[2vw] p-[6vw] w-full flex justify-between items-center">
         <span className="px-[2vw] py-[1vw] bg-white rounded-full md:flex md:items-center gap-3 shadow-sm font-bold text-sm  hidden">
-          Reports{" "}
-          
-            <TbReportSearch />
-        
+          Reports <TbReportSearch />
         </span>
         <Hamburger />
-        <span className="md:px-[2vw] md:py-[1vw] py-[3vw] px-[5vw] bg-white rounded-full flex  gap-3 shadow-sm font-bold text-sm items-center" >
+        <span className="md:px-[2vw] md:py-[1vw] py-[3vw] px-[5vw] bg-white rounded-full flex  gap-3 shadow-sm font-bold text-sm items-center cursor-pointer">
           Add New Analyst
           <CiSaveDown1 />
         </span>
       </div>
-
       <ProductsDeals />
     </div>
   );
