29add84fd4775560223e51302a3e4c2b58049eb8
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-26 12:12:19 +0530

    final commit

--- app/components/Hamburger.jsx
+++ app/components/Hamburger.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
+import gsap from "gsap";
+import { RiArrowLeftDoubleLine } from "react-icons/ri";
 
 const Hamburger = () => {
+  const menuRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    if (isOpen) {
+      gsap.to(menuRef.current, {
+        width: 0,
+        duration: 0.5,
+        ease: "power1",
+      });
+    } else {
+      gsap.to(menuRef.current, {
+        width: "100vw",
+        duration: 0.5,
+        ease: "power1",
+      });
+    }
+    setIsOpen(!isOpen);
+  };
   return (
     <div>
-      <div>
+      <div onClick={handleClick}>
         <RxHamburgerMenu size={20} />
       </div>
-      <div className="absolute top-0 left-0 h-full bg-red-800 w-[100vw]">
+      <div
+        ref={menuRef}
+        className="absolute top-0 left-0 h-full bg-[#45474B] w-0 overflow-hidden transition-width ease-in-out duration-500"
+      >
+        {isOpen && (
+          <RiArrowLeftDoubleLine
+          size={25}
+            style={{
+              position: "absolute",
+              top: "35px",
+              right: "20px",
+              zIndex: "1000",
+              color:'white'
+            }}
+            onClick={handleClick}
+          />
+        )}
         hu
       </div>
     </div>
