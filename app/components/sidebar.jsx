524462c1f1348d3a09b89316514a18ffce9b4203
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-26 10:07:25 +0530

    initial commit

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -8,6 +8,18 @@ const SideBar = () => {
           ECOMM <span className="text-2xl">DATA</span>
         </h1>
       </div>
+      <div className=" py-[1vw] px-[2vw]">
+        <h2 className="text-gray-400 opacity-20 font-mono text-xl uppercase">
+          Dashboard
+        </h2>
+      </div>
+      <hr className="w-full bg-white opacity-20" />
+      <div className=" py-[1vw] px-[2vw]">
+        <div>
+          <img src="" alt="" />
+          <p>Home</p>
+        </div>
+      </div>
     </div>
   );
 };
