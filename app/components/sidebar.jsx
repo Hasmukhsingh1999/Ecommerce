d5cc2abfd2649a00f25a37cdb1014dd2124cdac4
Hasmukh <hasmukhsingh90@gmail.com>
2023-11-26 10:28:49 +0530

    final commit

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -1,26 +1,72 @@
 import React from "react";
 import { GrHomeRounded } from "react-icons/gr";
+import { FaRegUser } from "react-icons/fa6";
+import { ImStatsBars } from "react-icons/im";
+import { IoFileTrayFullOutline } from "react-icons/io5";
+import { LuUserCircle2 } from "react-icons/lu";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosLogOut } from "react-icons/io";
 
+const useFullLinks = [
+  { id: 1, imageCompo: <GrHomeRounded />, text: "Overview" },
+  { id: 2, imageCompo: <FaRegUser />, text: "Memebers" },
+  { id: 3, imageCompo: <ImStatsBars />, text: "Stats" },
+  { id: 4, imageCompo: <IoFileTrayFullOutline />, text: "File Manager" },
+];
 
-const useFullLinks =[]
+const userProfile = [
+  { id: 1, imageCompo: <LuUserCircle2 />, text: "Profile" },
+  { id: 2, imageCompo: <IoSettingsOutline />, text: "Settings" },
+];
+
 const SideBar = () => {
   return (
-    <div className="w-[15vw] h-screen bg-[#191717] text-white">
-      <div className="p-[2vw]">
-        <h1 className="text-white font-serif text-sm">
-          ECOMM <span className="text-2xl">DATA</span>
-        </h1>
+    <div className="w-[15vw] h-screen bg-[#191717] text-white flex flex-col justify-between">
+      <div>
+        <div className="p-[2vw]">
+          <h1 className="text-white font-serif text-sm ">
+            ECOMM <span className="text-2xl">DATA</span>
+          </h1>
+        </div>
+        <div className=" py-[1vw] px-[2vw]">
+          <h2 className="text-gray-400 opacity-50 font-mono text-xl uppercase cursor-pointer hover:text-white hover:opacity-100 ">
+            Dashboard
+          </h2>
+        </div>
+        <hr className="w-full bg-white opacity-20" />
+        <div className=" py-[1vw] px-[2vw] flex flex-col gap-6">
+          {useFullLinks.map((item, id) => (
+            <div
+              className="flex items-center gap-5 opacity-50 hover:text-white hover:opacity-100 cursor-pointer"
+              key={id}
+            >
+              {item.imageCompo}
+              {item.text}
+            </div>
+          ))}
+        </div>
+        <hr className="w-full bg-white opacity-20" />
+        <div className=" py-[1vw] px-[2vw]">
+          <h2 className="text-gray-400 opacity-50 font-mono text-xl uppercase cursor-pointer hover:text-white hover:opacity-100 ">
+            Preference
+          </h2>
+        </div>
+        <div className=" py-[1vw] px-[2vw] flex flex-col gap-6">
+          {userProfile.map((item, id) => (
+            <div
+              className="flex items-center gap-5 opacity-50 hover:text-white hover:opacity-100 cursor-pointer"
+              key={id}
+            >
+              {item.imageCompo}
+              {item.text}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className=" py-[1vw] px-[2vw]">
-        <h2 className="text-gray-400 opacity-20 font-mono text-xl uppercase">
-          Dashboard
-        </h2>
-      </div>
-      <hr className="w-full bg-white opacity-20" />
-      <div className=" py-[1vw] px-[2vw] flex flex-col gap-6">
-        <div className="flex items-center gap-5">
-          <GrHomeRounded size={15}/>
-          <p className="text-xl">Home</p>
+      <div>
+        <div className=" py-[1vw] px-[2vw] flex  gap-6 items-center">
+          <IoIosLogOut />
+          <p>Logout</p>
         </div>
       </div>
     </div>
